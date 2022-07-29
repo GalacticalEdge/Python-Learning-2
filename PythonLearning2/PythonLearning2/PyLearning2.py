a32ce7622ad098f4c6c7c2444a447674ea9adcf3
Galactical Edge <31458662+GalacticalEdge@users.noreply.github.com>
2022-07-29 16:16:31 -0700

    Little fun project included
    I haven't added anything new to the PyLearning2.py file, but I have uploaded HackerSimScript.py as a little fun Python project.
    
    BUT BE SURE TO READ THE NOTE AT THE VERY TOP OF THE HackSimScript.py FILE BEFORE USING IT IN ANY PUBLIC PLACE

---
+++ PythonLearning2/PythonLearning2/PyLearning2.py
@@ -0,0 +1,46 @@
+if 14 > 13.5:
+    print("14 is greater than 13.5")
+
+x = 5
+x = "lifes left"
+
+print(x)
+
+# Left off on Python Variable Names
+
+myvariable = "Joey"
+
+print(myvariable)
+
+print("Here are some grouped value")
+
+ac, bc, cc = "Soda", "Juice", "Water"
+
+print(ac)
+print(bc)
+print(cc)
+
+print("Here is an unpacked collection")
+
+colors = ["red", "orange", "yellow"]
+aa, ab, ac = colors
+print(aa)
+print(ab)
+print(ac)
+
+print("Here are some output variables")
+
+zx = "Python"
+zy = "is"
+zz = "life"
+print(zx, zy, zz)
+
+print("Here is a global variable")
+
+rr = " cool"
+
+def myfunc():
+    print("Python is" + rr)
+myfunc()
+
+# Left off on Python Data Types
