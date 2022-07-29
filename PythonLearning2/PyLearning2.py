c1840eb0c457b3a6aa19415c1a6c58268c29fb90
Galactical Edge <31458662+GalacticalEdge@users.noreply.github.com>
2022-07-29 10:54:58 -0700

    Add files via upload

--- PythonLearning2/PyLearning2.py
+++ PythonLearning2/PyLearning2.py
@@ -7,3 +7,40 @@ x = "lifes left"
 print(x)
 
 # Left off on Python Variable Names
+
+myvariable = "Joey"
+
+print(myvariable)
+
+print("Here are some grouped value")
+
+ac, bc, cc = "Soda", "Juice", "Water"
+
+print(ac)
+print(bc)
+print(cc)
+
+print("Here is an unpacked collection")
+
+colors = ["red", "orange", "yellow"]
+aa, ab, ac = colors
+print(aa)
+print(ab)
+print(ac)
+
+print("Here are some output variables")
+
+zx = "Python"
+zy = "is"
+zz = "life"
+print(zx, zy, zz)
+
+print("Here is a global variable")
+
+rr = " cool"
+
+def myfunc():
+    print("Python is" + rr)
+myfunc()
+
+# Left off on Python Data Types
