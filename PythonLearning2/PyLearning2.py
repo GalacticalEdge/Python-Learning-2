0f91ff941d8fc932815e2591e74d7202a3239530
Galactical Edge <31458662+GalacticalEdge@users.noreply.github.com>
2022-07-30 11:00:51 -0700

    Add files via upload

---
+++ PythonLearning2/PyLearning2.py
@@ -0,0 +1,58 @@
+if 14 > 13.5:
+    print("14 is greater than 13.5")
+
+x = 5
+x = "lifes left"
+y = 3.545453
+z = 1j
+
+print(x)
+
+# Left off on Python Variable Names
+
+myvariable = "Joey"
+
+print(myvariable)
+
+print("Here are some grouped value")
+
+ac, bc, cc = "Soda", "Juice", "Water"
+
+print(ac)
+print(bc)
+print(cc)
+
+print("Here is an unpacked collection")
+
+colors = ["red", "orange", "yellow"]
+aa, ab, ac = colors
+print(aa)
+print(ab)
+print(ac)
+
+print("Here are some output variables")
+
+zx = "Python"
+zy = "is"
+zz = "life"
+print(zx, zy, zz)
+
+print("Here is a global variable")
+
+rr = " cool"
+
+def myfunc():
+    print("Python is" + rr)
+myfunc()
+
+# Left off on Python Data Types
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+import random
+
+print(random.randrange(1, 10))
+
+# Left off on Python Casting
