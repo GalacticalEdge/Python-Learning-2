bf2ad26ece7e22f8f75e09eb524746079dd08eac
Galactical Edge <31458662+GalacticalEdge@users.noreply.github.com>
2022-07-31 13:36:54 -0700

    Add files via upload

--- PythonLearning2/PyLearning2.py
+++ PythonLearning2/PyLearning2.py
@@ -56,3 +56,36 @@ import random
 print(random.randrange(1, 10))
 
 # Left off on Python Casting
+
+yy = int(3.5) # This number will be 3 since it's looking for an integer
+print(yy)
+
+w = float(1) # This number will 1.0 since float adds a decimal to the number
+print(w)
+
+g = str("s7") # This does not affect the number regardless of it's format
+print(g)
+
+p = "Hello, World!"
+print(p[0]) # This will print out a letter from 'p' depending on the variable in this line
+
+pp = "Hello, World!"
+print(pp[random.randrange(-1, 13)]) # And for fun, let's add this random letter thingy from earlier
+
+for uu in "apples": # This will print out the word "apples" vertically instead of horizontally
+    print(uu)      
+
+print(len(p)) # This will tell us the length of the variable 'p' which is "Hello, World!"
+
+txt = "The best things in life are free!" 
+print("free" in txt) # This will tell us if there is the world "free" in the variable 'txt'
+
+if "free" in txt:
+    print("Yes, 'free' is present") # But for something better, we can have it print out what we ask it for if the world "free" is present
+
+print("expensive" not in txt) # This will tell us if the word "expensive" is not in the variable 'txt'
+
+if "expensive" not in txt:
+  print("No, 'expensive' is not present in the variable 'txt'.") # But again, we can tell it to print something we want if the word "expensive" is not present
+
+# Left off on Python Slicing Strings 
